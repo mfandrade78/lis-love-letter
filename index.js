ae4b96e96f4a145633052be56442fab53dbc2b9c
matheusdev <164523758+mfandrade78@users.noreply.github.com>
2025-10-01 11:18:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+//Criação das variáveis de animação
 $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
 
+  //Função de clicar no envelope - vai fazer o envelope abrir
   envelope.click(function () {
     open();
   });
+  //Função de clicar no botão de abrir - vai fazer o envelope abrir
   btn_open.click(function () {
     open();
   });
+  //Função de clicar no botão de fechar - vai fazer o envelope fechar
   btn_reset.click(function () {
     close();
   });
 
+  //Instanciação da função abrir
   function open() {
     envelope.addClass("open").removeClass("close");
   }
+  //Instanciação da função fechar
   function close() {
     envelope.addClass("close").removeClass("open");
   }
 });
+
